69144946c05fbb37f3e78094cc441b997d620450
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-18 13:58:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,26 +14,40 @@ function convertToRoman(num) {
       7:['I', 1],
 		
     };
-	var ans="";
-	while(num!==0){
-		for(let j of obj){
-			if(num>=j[1]){
-				ans+=j[0];
-				num-=j[1];
-				break;
-			}
-		}
+// 	var ans="";
+// 	while(num!==0){
+// 		for(let j of obj){
+// 			if(num>=j[1]){
+// 				ans+=j[0];
+// 				num-=j[1];
+// 				break;
+// 			}
+// 		}
 		 
-}
-console.log(ans);
-}
-  //your code here
+// }
+// console.log(ans);
+// }
+//   //your code here
 
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
  //console.log(convertToRoman(36));
 
+let ans = "";
+  
+  // Loop through the Roman numerals and subtract the values from num
+  for (let [roman, value] of obj) {
+    while (num >= value) {
+      ans += roman;
+      num -= value;
+    }
+  }
 
+  return ans;
+}
+
+// Test the function
+console.log(convertToRoman(36));
 
 
 // do not edit below this line
